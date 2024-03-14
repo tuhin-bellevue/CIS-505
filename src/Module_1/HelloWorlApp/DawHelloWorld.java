d4d73d2935109d687a3cb99d5706a77277969c47
“tudaw@my365.bellevue.edu” <tudaw@my365.bellevue.edu>
2024-03-13 23:30:43 -0400

    Committing java assignment for module 1.2

---
+++ src/Module_1/HelloWorlApp/DawHelloWorld.java
@@ -0,0 +1,13 @@
+package Module_1.HelloWorlApp;
+
+public class DawHelloWorld {
+
+    public static void main(String[] args) {
+
+        String firstName = "Tuhin";
+        String lastName = "Daw";
+
+        System.out.printf("Hello World from" + " " + firstName + " " +lastName);
+
+    }
+}
