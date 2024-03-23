a3bc109f9196a90b372f6cb8eace73002bc7bece
“tudaw@my365.bellevue.edu” <tudaw@my365.bellevue.edu>
2024-03-23 18:31:10 -0400

    Assignment for Module 2.2. Creating JAR files, Added comments

--- src/Module_2/JarPracticeFiles/DawJarOne.java
+++ src/Module_2/JarPracticeFiles/DawJarOne.java
@@ -1,4 +1,12 @@
 package Module_2.JarPracticeFiles;
+
+/*
+    Daw, Tuhin(2024), CIS-505 Intermediate Java Programming. Bellevue University, all rights reserved.
+ */
+
+/**
+ * Sample java class that will be packaged inside a JAR file
+ */
 public class DawJarOne {
 
 }
--- src/Module_2/JarPracticeFiles/DawJarTwo.java
+++ src/Module_2/JarPracticeFiles/DawJarTwo.java
@@ -1,4 +1,11 @@
 package Module_2.JarPracticeFiles;
 
+/*
+    Daw, Tuhin(2024), CIS-505 Intermediate Java Programming. Bellevue University, all rights reserved.
+ */
+
+/**
+ * Sample java class that will be packaged inside a JAR file
+ */
 public class DawJarTwo {
 }
